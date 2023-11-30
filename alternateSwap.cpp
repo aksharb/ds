ceaed98c9b45c6e5a4dc60f4447af53234ee1a92
Akshar Bisht <bisht.akshar@gmail.com>
2023-11-30 18:04:02 +0530

    implemented using fucntion

--- alternateSwap.cpp
+++ alternateSwap.cpp
@@ -1,13 +1,7 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main()
-{
-    vector<int> v(8);
-    cout<<"Enter the vector elements\n";
-    for(int i = 0; i < 8; i++){
-        cin>>v[i];
-    }
+void swapAlternate(vector<int> v, int size){
     for(int i = 0; i < 8; i = i + 2){
         swap(v[i], v[i+1]);
     }
@@ -15,5 +9,15 @@ int main()
     for(int i = 0; i < 8; i++){
         cout<<v[i]<<" ";
     }
+}
+int main()
+{
+    vector<int> v(8);
+    cout<<"Enter the vector elements\n";
+    for(int i = 0; i < 8; i++){
+        cin>>v[i];
+    }
+    swapAlternate(v,8);
+
     return 0;
 }
